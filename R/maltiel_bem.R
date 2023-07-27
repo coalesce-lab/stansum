f9b2fad5a381c6062378e0cac9779e82b1f65093
Michal Bojanowski <michal2992@gmail.com>
2023-07-27 18:59:20 +0200

    Add objects for models of Maltiel et al

---
+++ R/maltiel_bem.R
@@ -0,0 +1,52 @@
+#' Maltiel et al (2015) Barrier Effects Model
+#'
+#' @name maltiel_bem
+#'
+#' @template format-CmdStanModel
+#'
+#' @references Baum, D. S., & Marsden, P. V. (2023). Uses and limitations of
+#'   dichotomous aggregate relational data. Social Networks, 74, 42–61.
+#'   <https://doi.org/10.1016/j.socnet.2023.02.001>
+#'
+#' Maltiel, R., Raftery, A. E., McCormick, T. H., & Baraff, A. J.
+#'   (2015). Estimating Population Size Using the Network Scale Up Method. The
+#'   Annals of Applied Statistics, 9(3), 1247–1277.
+#'   <https://doi.org/10.1214/15-AOAS827>
+#'
+#' @importFrom instantiate stan_package_model2
+#'
+#' @family Models of Maltiel et al (2015)
+
+#' @rdname maltiel_bem
+#'
+#' @description - `maltiel_bem_count` -- model for count responses
+#' @format NULL
+#' @export
+maltiel_bem_count <- {
+  p <- system.file("stan", "MaltielBEM_count.stan", package = "stansum")
+  stan_package_model2(p)
+}
+
+
+#' @rdname maltiel_bem
+#'
+#' @description - `maltiel_bem_dichotomous` -- model for dichotmous responses
+#'   proposed by Baum & Marsden (2023).
+#' @format NULL
+#' @export
+maltiel_bem_dichotomous <- {
+  p <- system.file("stan", "MaltielBEM_dichotomous.stan", package = "stansum")
+  stan_package_model2(p)
+}
+
+
+#' @rdname maltiel_bem
+#'
+#' @description - `maltiel_bem_trichotomous` -- model for trichotomous responses
+#'   proposed by Baum & Marsden (2023).
+#' @format NULL
+#' @export
+maltiel_bem_trichotomous <- {
+  p <- system.file("stan", "MaltielBEM_trichotomous.stan", package = "stansum")
+  stan_package_model2(p)
+}
