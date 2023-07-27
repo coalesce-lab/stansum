150b38dd2a46c608a8a342bdeba877bc7a51aadc
Michal Bojanowski <michal2992@gmail.com>
2023-07-27 19:03:56 +0200

    Automatic package man page

--- R/stansum-package.R
+++ R/stansum-package.R
@@ -1,15 +1,6 @@
-#' Bayesian Models for Degree Estimation from Aggregated Relational Data
-#'
-#' @description Interface to various precompiled Stan models for Aggregated
-#'   Relational Data that estiamte, among other things, network degrees and
-#'   other things.
-#'
-#' @docType package
-#' @name stansum-package
-#' @aliases stansum
-#'
-#' @references
-#' Stan Development Team (2023). RStan: the R interface to Stan. R package
-#' version 2.21.8. https://mc-stan.org
-#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
