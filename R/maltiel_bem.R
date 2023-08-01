b58732282cc4d876d6094e004a84b40380c946ee
Michal Bojanowski <michal2992@gmail.com>
2023-08-01 12:15:12 +0200

    Change interface functions
    The R functions call $sample() method of the model object.

--- R/maltiel_bem.R
+++ R/maltiel_bem.R
@@ -2,28 +2,38 @@
 #'
 #' @name maltiel_bem
 #'
-#' @template format-CmdStanModel
-#'
 #' @references Baum, D. S., & Marsden, P. V. (2023). Uses and limitations of
-#'   dichotomous aggregate relational data. Social Networks, 74, 42–61.
+#'   dichotomous aggregate relational data. *Social Networks*, 74, 42–61.
 #'   <https://doi.org/10.1016/j.socnet.2023.02.001>
 #'
 #' Maltiel, R., Raftery, A. E., McCormick, T. H., & Baraff, A. J.
-#'   (2015). Estimating Population Size Using the Network Scale Up Method. The
-#'   Annals of Applied Statistics, 9(3), 1247–1277.
+#'   (2015). Estimating Population Size Using the Network Scale Up Method. *The
+#'   Annals of Applied Statistics*, 9(3), 1247–1277.
 #'   <https://doi.org/10.1214/15-AOAS827>
 #'
 #' @importFrom instantiate stan_package_model
 #'
 #' @family Models of Maltiel et al (2015)
 
+
+
+
 #' @rdname maltiel_bem
 #'
 #' @description - `maltiel_bem_count` -- model for count responses
-#' @format NULL
+#'
+#' @param N integer; population size
+#' @param K integer; number of sub-populations
+#' @param y integer matrix; ARD
+#' @param m numeric; vector of fractional sizes of sub-populations
+#' @param L numeric; lower bound on degree
+#' @param ... other arguments passed to [cmdstanr::sample()]
+#'
 #' @export
-maltiel_bem_count <- {
-  p <- system.file("stan", "MaltielBEM_count.stan", package = "stansum")
+maltiel_bem_count <- function(N, K, y, m, L, ...) {
+  mod <- get_model("MaltielBEM_count")
+  d <- list(N=N, K=K, y=y, m=m, L=L)
+  mod$sample(data = d, ...)
 }
 
 
@@ -31,10 +41,11 @@ maltiel_bem_count <- {
 #'
 #' @description - `maltiel_bem_dichotomous` -- model for dichotmous responses
 #'   proposed by Baum & Marsden (2023).
-#' @format NULL
 #' @export
-maltiel_bem_dichotomous <- {
-  p <- system.file("stan", "MaltielBEM_dichotomous.stan", package = "stansum")
+maltiel_bem_dichotomous <- function(N, K, y, m, L, ...) {
+  mod <- get_model("MaltielBEM_dichotomous")
+  d <- list(N=N, K=K, y=y, m=m, L=L)
+  mod$sample(data = d, ...)
 }
 
 
@@ -44,6 +55,8 @@ maltiel_bem_dichotomous <- {
 #'   proposed by Baum & Marsden (2023).
 #' @format NULL
 #' @export
-maltiel_bem_trichotomous <- {
-  p <- system.file("stan", "MaltielBEM_trichotomous.stan", package = "stansum")
+maltiel_bem_trichotomous <- function(N, K, y, m, L, ...) {
+  mod <- get_model("MaltielBEM_trichotomous")
+  d <- list(N=N, K=K, y=y, m=m, L=L)
+  mod$sample(data = d, ...)
 }
