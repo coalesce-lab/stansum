26e3f25cbd8a3f80f62a23b651a6d85e9f370f07
Michal Bojanowski <michal2992@gmail.com>
2023-08-01 13:18:09 +0200

    More docs cleanups
    Remove common References section.

--- man-roxygen/wrapped-stan-model.R
+++ man-roxygen/wrapped-stan-model.R
@@ -1,11 +1,9 @@
 #'
 #' @param ... other arguments passed to [cmdstanr::sample()]
 #'
-#' @details Calling this function will load the model object using [get_model()]
-#'   and sample from the posterior using the [cmdstanr::sample()] method.
-#'
-#'   See Laga et al (2021) for a review of other models for Aggregated
-#'   Relational Data (ARD).
+#' @details Calling this function(s) will load the model object using
+#'   [get_model()] and sample from the posterior using the [cmdstanr::sample()]
+#'   method.
 #'
 #' @returns Object of class [cmdstanr::CmdStanMCMC] containing the samples.
 #'
@@ -13,8 +11,4 @@
 #'   [cmdstanr::CmdStanModel] without fitting it.
 #'
 #' @family Models for ARD
-#'
-#' @references Laga, I., Bao, L., & Niu, X. (2021). Thirty Years of The Network
-#'   Scale-up Method. *Journal of the American Statistical Association*,
-#'   116(535), 1548–1559.
 #'
