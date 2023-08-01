97546de09282c458609b2b02c93cdc946a88567f
Michal Bojanowski <michal2992@gmail.com>
2023-08-01 11:08:24 +0200

    Add list_models() and get_model()

---
+++ R/get_model.R
@@ -0,0 +1,35 @@
+#' List or get Stan model objects
+#'
+#' List Stan models available in this package or load them in the form of
+#' `CmdStanModel` objects as implemented in the \pkg{cmdstanr} package.
+#'
+#' @param name character; name of the model to load
+#' @param ... arguments passed to [instantiate::stan_package_model]
+#'
+#' @returns `get_model` returns an "CmdStanModel" object.
+#'
+#' @importFrom instantiate stan_package_model
+#'
+#' @export
+get_model <- function(name, ...) {
+  stopifnot(name %in% list_models())
+  stan_package_model(name = name, package = "stansum", ...)
+}
+
+
+#' @rdname get_model
+#'
+#' @return `list_models` returns a character vector of model names provided
+#'   with the package.
+#'
+#' @export
+list_models <- function() {
+  gsub(
+    "\\.stan$",
+    "",
+    list.files(
+      path = system.file("stan", package = "stansum"),
+      pattern = "\\.stan$"
+    )
+  )
+}
