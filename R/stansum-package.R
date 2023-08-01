7e8e621470daafdb75670e36c888be6102f30b24
Michal Bojanowski <michal2992@gmail.com>
2023-08-01 13:48:37 +0200

    Add references to package man page

--- R/stansum-package.R
+++ R/stansum-package.R
@@ -1,6 +1,24 @@
+#'
 #' @keywords internal
+#'
+#' @references Baum, D. S., & Marsden, P. V. (2023). Uses and limitations of
+#'   dichotomous aggregate relational data. *Social Networks*, 74, 42–61.
+#'   \doi{10.1016/j.socnet.2023.02.001}
+#'
+#'   Maltiel, R., Raftery, A. E., McCormick, T. H., & Baraff, A. J. (2015).
+#'   Estimating Population Size Using the Network Scale Up Method. *The Annals
+#'   of Applied Statistics*, 9(3), 1247–1277.
+#'   \doi{10.1214/15-AOAS827}
+#'
+#'   Zheng, T., Salganik, M. J., & Gelman, A. (2006). How Many People Do You
+#'   Know in Prison? Using Overdispersion in Count Data to Estimate Social
+#'   Structure in Networks. Journal of the American Statistical Association,
+#'   101(474), 409–423. \doi{10.1198/016214505000001168}
+#'
+#'
 "_PACKAGE"
 
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
